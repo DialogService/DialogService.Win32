f909dbbb7ec6435a882b6ccd4e1cee406550e635
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-05-04 23:05:36 +0300

    Add some testing stuff

---
+++ DialogService.Win32.TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using DialogService.Items;
+using System;
+
+namespace DialogService.Win32.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testDialog = TestDialog.GetDialog();
+            var service = new DialogPlatformBuilder()
+                .AddPlatform<PlatformImplementation>()
+                .GetService();
+
+            var result = service.Show(testDialog);
+
+            service.ShowMessageBox(TestDialog.GetClosedBy(result), "Win32 Test");
+        }
+    }
+}
